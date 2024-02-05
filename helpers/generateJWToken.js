4052a6f7ec3910bde846f6436b2e55d3aaa72bd0
Hizaram <promisevector@gmail.com>
2024-02-05 23:17:30 +0100

    [Feat]: Added the module that generates the JWToken using the secret key

---
+++ helpers/generateJWToken.js
@@ -0,0 +1,10 @@
+const jwt = require('jsonwebtoken');
+
+const generateJWToken = (customerId) => {
+  const token = jwt.sign({ customerId }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+  return token;
+};
+
+module.exports = generateJWToken;
